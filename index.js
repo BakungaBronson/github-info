3dcbe0db205da074f5495c759f16ff0e386cb54c
BakungaBronson <bakungabro@gmail.com>
2022-09-25 22:50:54 +0300

    Added function to get 100 users

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const { Octokit } = require("@octokit/core");
+require('dotenv').config();
 
+(async () => {
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN,
+    });
+    const debug = true;
+    const users = await octokit
+        .request("GET /search/users", {
+            q: "location:uganda",
+            // Set the number of users to retrieve
+            per_page: 100,
+        })
+        .then((response) => {
+            return response.data.items;
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log("Error fetching users: Please check that .env file is set up correctly with your github token");
+        });
+    if (debug) console.log(users.length);
+})();
