2515c14b827ac7c2ebf5b8d9cc4a44212cbbd965
BakungaBronson <bakungabro@gmail.com>
2022-09-26 09:51:16 +0300

    Returns the first 1000 matches

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const { Octokit } = require("@octokit/core");
-require('dotenv').config();
+require("dotenv").config();
 
 (async () => {
     const octokit = new Octokit({
         auth: process.env.GITHUB_TOKEN,
     });
     const debug = true;
-    const users = await octokit
-        .request("GET /search/users", {
-            q: "location:"+process.env.LOCATION,
-            // Set the number of users to retrieve
-            per_page: 100,
-        })
-        .then((response) => {
-            return response.data.items;
-        })
-        .catch((error) => {
-            console.log("Error:", error.status, error.message);
-        });
-    if (users && debug) console.log(users.length);
+    let users = [];
+    const trialLength = 11;
+
+    for (let index = 1; index < trialLength; index++) {
+        await octokit
+            .request("GET /search/users", {
+                q: "location:" + process.env.LOCATION,
+                // Set the number of users to retrieve
+                per_page: 100,
+                page: index,
+            })
+            .then((response) => {
+                users = [...users, ...response.data.items];
+            })
+            .catch((error) => {
+                console.log("Error:", error.status, error.message);
+            });
+    }
+    if (users && debug) console.log("Users' array contains: " + users.length);
 })();
